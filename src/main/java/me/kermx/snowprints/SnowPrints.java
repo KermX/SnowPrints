8d0aa01e1f3d98215a6b3749597862122eb492ab
KermX <KermitVIII@gmail.com>
2023-03-09 03:42:36 -0600

    Initial commit

---
+++ src/main/java/me/kermx/snowprints/SnowPrints.java
@@ -0,0 +1,44 @@
+package me.kermx.snowprints;
+
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Random;
+
+public final class SnowPrints extends JavaPlugin {
+    private static final Random random = new Random();
+    @Override
+    public void onEnable() {
+        Bukkit.getLogger().info("SnowPrints enabled successfully!");
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            @Override
+            public void run() {
+                for (Player player : Bukkit.getOnlinePlayers())
+                    SnowPrints.this.handlePlayer(player);
+            }
+        }, 10L, 5L);
+
+    }
+
+    private void handlePlayer(Player player) {
+        if (player.getGameMode() != GameMode.SURVIVAL || player.isSneaking())
+            return;
+        handleBlock(player.getLocation().getBlock(), player);
+    }
+
+    private void handleBlock(Block block, Player player) {
+        if (block.getType() == Material.SNOW && player.isSprinting())
+            block.setType(Material.AIR);
+        if (block.getType() == Material.SNOW && random.nextInt(200) < 60)
+            block.setType(Material.AIR);
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getLogger().info("§bSnowPrints disabled!");
+    }
+}
